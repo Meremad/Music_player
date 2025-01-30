f9303edbda82ed87d961f02f70dcfb87f3cb32ed
Nap20192 <kogay20192@gmail.com>
2025-01-30 16:26:31 +0500

    asdа

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err))
 
